87ac423c6b188748b8a56fdc62f2b4c13d557055
Aquos06 <ruliciohansen@gmail.com>
2024-08-03 20:35:08 +0800

    feat(analyze): add dieases hypotessis

--- page/admin/analyze.py
+++ page/admin/analyze.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 from utils.database.graph import Neo4Graph
 from utils.inference.service import AI71Inference
 
@@ -18,36 +19,93 @@ def get_patient_summary(patient_name, patient_id):
     patient_summary = AI71.make_suggestion(history=messages)
     return patient_summary
 
+def get_patient_hypotesis(patient_name, patient_id, patient_complaint):
+    cypher_patient = [f""" 
+            MATCH (p:Patient {{id: '{patient_id}'}})-[r]-(related)
+            WHERE NOT type(r) = 'HAS_MEDICAL_HISTORY'
+            RETURN p, r, related
+              """]
+    cypher_record = [f"""
+            MATCH (p:Patient {{patient_id: "{patient_id}", full_name: "{patient_name}"}})-[r:HAS_MEDICAL_HISTORY]->(mh:MedicalHistory)
+            RETURN p, r, mh
+            ORDER BY r.date ASC
+                     """]
+    
+    graph = Neo4Graph()
+    AI71 = AI71Inference()
+    record_patient = graph.read_transaction(queries=cypher_patient)
+    st.session_state.patient_information = record_patient
+    patient_medical = graph.read_transaction(queries=cypher_record)
+    patient_medical = patient_medical[:-1]
+    st.session_state.patient_medical_history = patient_medical
+    
+    messages = [{"role":"system","content":"You are an AI assistant designed to help doctor to diagnoze patient's diesease from the patient's information and past medical history. Always focus in the patient complaint"},
+                {"role":"user", "content": f"""Using the patient information and patient complaint, Diagnosed patient's.
+                                               Patient Information: {record_patient} {patient_medical}
+                                               Patient Complaint: {patient_complaint}"""}]
+    patient_diagnose = AI71.make_suggestion(history=messages)
+    return patient_diagnose
+
+def get_patient_medicines(patient_dieases):
+    messages = [{"role": "system","content": "You are an AI assistant designed to help doctor to decide patient's medicines according to its information and current dieases"},
+                {"role": "user","content": f"""Given a patient's dieases and patient's information. Give me a list of medicines and the rules for using the medicines.
+                                               Patient Information: {st.session_state.patient_information}
+                                               Patient Past Medical Record: {st.session_state.patient_medical_history}
+                                               Patient Dieases: {patient_dieases}"""}]
+    
+    AI71 = AI71Inference()
+    medicine_list = AI71.make_suggestion(history=messages)
+    return medicine_list
+    
+
 def analyze_ui():
+    if "patient_information" not in st.session_state:
+        st.session_state.patient_information = ""
+    if "patient_medical_history" not in st.session_state:
+        st.session_state.patient_medical_history = ""
+    if "patient_summary" not in st.session_state:
+        st.session_state.patient_summary = ""
+    if "patient_diagnose" not in st.session_state:
+        st.session_state.patient_diagnose = ""
+    if "patient_medicine_list" not in st.session_state:
+        st.session_state.patient_medicine_list = ""
     if "patient_name" not in st.session_state:
-        st.session_state.patient_name= ""
+        st.session_state.patient_name=""
+    if "patient_id" not in st.session_state:
+        st.session_state.patient_id=""
     if "patient_complaint" not in st.session_state:
         st.session_state.patient_complaint = ""
-    if "patient_drugs" not in st.session_state:
-        st.session_state.patient_drugs = ""
-    if "patient_diseases" not in st.session_state:
-        st.session_state.patient_diseases = ""
+    if "patient_dieases" not in st.session_state:
+        st.session_state.patient_dieases = ""
         
     patient_name = st.text_input("Patient Name")
     patient_id = st.text_input("Patient Id")
-    if patient_name and patient_id:
+    if patient_name and patient_id and (st.session_state.patient_summary == "" or st.session_state.patient_name != patient_name or st.session_state.patient_id != patient_id):
         st.session_state.patient_name = patient_name
+        st.session_state.patient_id = patient_id
         patient_summary = get_patient_summary(patient_name=patient_name, patient_id=patient_id)
+        st.session_state.patient_summary = patient_summary
+        st.markdown(f"Patient Information Summary")
         st.markdown(patient_summary)
     else:
         st.markdown(f"Patient Summary Loading...")
     
     patient_complaint = st.text_input("Type the patient's complaint")
-    if patient_complaint:
-        st.markdown("Analyze Dieases Loading...")
+    if patient_complaint  and patient_id and (st.session_state.patient_diagnose == "" or st.session_state.patient_complaint != patient_complaint):
         st.session_state.patient_complaint = patient_complaint
+        patient_diagnose = get_patient_hypotesis(patient_name=patient_name, patient_id=patient_id, patient_complaint=patient_complaint)
+        st.session_state.patient_diagnose = patient_diagnose
+        st.markdown(f"Patient Diagnose")
+        st.markdown(f"{patient_diagnose}")
+    else:
+        st.markdown("Analyze Dieases Loading...")
         
     patient_diseases = st.text_input("Type Final Diseases")
-    if patient_diseases:
+    if patient_diseases and (st.session_state.patient_medicine_list == "" or st.session_state.patient_dieases != patient_diseases) :
         st.session_state.patient_diseases = patient_diseases
-    
-    patient_drugs = st.text_input("Type Patient Drugs")
-    if patient_drugs:
-        st.session_state.patient_drugs = patient_drugs
-        
-    export_drugs_document = st.button("Export Report")
+        medicine_list = get_patient_medicines(patient_dieases=patient_diseases)
+        st.session_state.patient_medicine_list = medicine_list
+        st.markdown("Medicine Suggestion List")
+        st.markdown(medicine_list)
+    else:
+        st.markdown("Medicine List Suggestion Loading...")
