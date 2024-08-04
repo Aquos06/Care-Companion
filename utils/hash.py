8c7b050718f39be378d2977d4444d531942e143b
Aquos06 <ruliciohansen@gmail.com>
2024-08-04 23:57:37 +0800

    fix: put to string

--- utils/hash.py
+++ utils/hash.py
@@ -2,8 +2,8 @@ import bcrypt
 
 def hash_password(password):
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'),salt)
+    hashed_password = bcrypt.hashpw(str(password).encode('utf-8'),salt)
     return hashed_password
 
 def verify_password(stored_password, provided_password):
-    return bcrypt.checkpw(provided_password.encode('utf-8'), stored_password.encode('utf-8'))
+    return bcrypt.checkpw(str(provided_password).encode('utf-8'), str(stored_password).encode('utf-8'))
