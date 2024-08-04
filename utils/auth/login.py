e54990da28884def51331f7f05d177b4dd295ed8
Aquos06 <ruliciohansen@gmail.com>
2024-08-04 23:32:36 +0800

    fix: login

--- utils/auth/login.py
+++ utils/auth/login.py
@@ -5,7 +5,10 @@ def login(email:str, password: str):
     graph = Neo4Graph()
     cypher = [f"MATCH (p:Patient {{email_addr: '{email}'}}) RETURN p.password"]
     patient_password = graph.read_transaction(cypher)
-    if patient_password:
-        pwd = patient_password[0][0]['p.password']
-        return verify_password(stored_password=pwd, provided_password=password)
-    return False
+    try:
+        if patient_password[0]:
+            pwd = patient_password[0][0]['p.password']
+            return verify_password(stored_password=pwd, provided_password=password)
+        return False
+    except:
+        return False
